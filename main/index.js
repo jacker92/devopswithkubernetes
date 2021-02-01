1c4e927d10b9b287c39d6ec15cd24f1cc70626b6
jacker92 <lahtinen.jaakko@gmail.com>
2021-02-01 20:30:58 +0200

    Added task 1.7

--- main/index.js
+++ main/index.js
@@ -1,7 +1,18 @@
 import { v4 as uuidv4 } from 'uuid';
+import express from 'express'
 
+const app = express()
+const port = 3000
 const value = uuidv4();
 
+app.get('/', (req, res) => {
+    res.send(`${new Date().toISOString()}: ${value}`)
+})
+
 setInterval(() => {
-    console.log(`${new Date().toISOString()}: ${value}`) 
+    console.log(`${new Date().toISOString()}: ${value}`)
 }, 5000)    
+
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`)
+  })
