fdb9435c8f811d6b07682cf9a926065f3be5d1df
jacker92 <lahtinen.jaakko@gmail.com>
2021-02-06 20:22:12 +0200

    Added task 1.12

--- todo-app/index.js
+++ todo-app/index.js
@@ -1,6 +1,26 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const fs = require('fs')
+const axios = require('axios')
+
+const download_image = (url, image_path) => {
+  console.log("Downloading image")
+  axios({
+    url,
+    responseType: 'stream',
+  }).then(
+    response =>
+      new Promise((resolve, reject) => {
+        response.data
+          .pipe(fs.createWriteStream(image_path))
+          .on('finish', () => resolve())
+          .on('error', e => reject(e));
+      }),
+  );
+}
+
+download_image('https://picsum.photos/1200', 'public/images/1200.jpg')
 
 app.use(express.static('public'))
 app.listen(port, () => {
